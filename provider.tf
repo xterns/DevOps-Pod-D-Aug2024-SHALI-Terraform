7dd879bb73310cbea222545363b724501ccf745d
Aduraleke Faith Akintade <103920213+leketech@users.noreply.github.com>
2024-09-18 20:22:17 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,13 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # To maintain backward compatibility with 5.x versions
+      version = "~> 4.16"
     }
   }
+  required_version = ">= 1.2.0"
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1" 
+  region = var.aws_region
 }
-
