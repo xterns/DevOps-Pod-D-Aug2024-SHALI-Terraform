9b91fd803e47388780af2feae45f8bc86a6265a5
sanmi-jacob <jacob.isedowo@yahoo.co.uk>
2024-10-15 21:51:41 +0100

    ‘feature/modules’

---
+++ Terraform/modules/ALB-NLB/main.tf
@@ -0,0 +1,8 @@
+
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIA5U23ZSZWW7U4OAMW"
+    secret_key = "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"
+
+}
--- provider.tf
+++ provider.tf
@@ -1,13 +1,9 @@
-terraform {
+ terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+      source = "hashicorp/aws"
+      version = "5.69.0"
     }
   }
-  required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region = var.aws_region
-}
