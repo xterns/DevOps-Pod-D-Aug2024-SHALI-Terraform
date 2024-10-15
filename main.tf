a0af6d63040d75457f9b30bcef49c5f9f96b4e54
Aduraleke Faith Akintade <103920213+leketech@users.noreply.github.com>
2024-10-15 13:51:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,30 +7,27 @@ resource "aws_security_group" "ec2_security_group" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["10.0.1.0/16"]
   }
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
   ingress {
     description      = "https"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["10.0.1.0/16"]
   }
   ingress {
     description      = "http"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["10.0.1.0/16"]
   }
   egress {
     from_port        = 0
