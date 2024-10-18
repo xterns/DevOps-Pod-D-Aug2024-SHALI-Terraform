be5a9a1fe9f42f9e8d89fe722656a7e214e3d286
leketech <aaduraleke@aol.com>
2024-10-18 15:30:24 +0100

    ggit merge

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -20,11 +20,16 @@ resource "aws_subnet" "main" {
 
 data "aws_ami" "latest_packer" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-20240927"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
