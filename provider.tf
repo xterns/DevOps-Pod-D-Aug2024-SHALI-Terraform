935da23b1f96e832e6c6a19520b645296e67db96
Aduraleke Faith Akintade <103920213+leketech@users.noreply.github.com>
2024-11-06 17:49:17 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,16 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  backend "s3" {
-    bucket = "my-bucket"
-  }
-  required_version = ">= 1.2.0"
-}
+#terraform {
+ # required_providers {
+  #  aws = {
+     # source  = "hashicorp/aws"
+    #  version = "~> 4.16"
+   # }
+ # }
+ # backend "s3" {
+   # bucket = "my-bucket"
+ # }
+ # required_version = ">= 1.2.0"
+#}
 
-provider "aws" {
-  region = var.aws_region
-}
+#provider "aws" {
+ # region = var.aws_region
+#}
